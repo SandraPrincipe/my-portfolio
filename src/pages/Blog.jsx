db4fe78a81c8bfbef06afe9e9f4ffde8899782d9
Sandra Principe <principesandra311@gmail.com>
2026-04-16 22:23:19 -0700

    Initial portfolio commit

---
+++ src/pages/Blog.jsx
@@ -0,0 +1,97 @@
+function Blog() {
+    return(
+        <main className="section">
+            <h2>My Blog</h2>
+            <p>Welcome to my blog! This is where I'll document my spring break adventures.</p>
+
+            <article className="blog-post">
+                <h3>Spring Break 2026</h3>
+        
+            </article>
+            <section id="post" className="blog-post">
+                <h5> Day 1: 3/28/26</h5>
+                <img src="./images/buckeesign.jpg" alt="buckeesSign"/>
+                <p>
+                    One of my first days of spring break, I went to Texas to visit my older brother!
+                    The first day I was there, we took a trip to see a famous, HUGE gas station called Buckees.
+                    It was basically like a grocery store but also a gas station.</p>
+                <p>
+                    As they say  "Everything is bigger in Texas!". Let me give a list of all the stuff they have. Beef jerky, chips, peanuts, a whole cafeteria, deserts, clothes,
+                    jewelery, candy, and of course, gas. When we went, there were so many people and I can see why. It felt like a 
+                    whole experience thats similar to an amusement park.
+                    Below is a Buckee plushie I got!
+                </p>
+                <img src="./images/buckee.jpg" alt="buckeesPlush" />
+            </section>
+
+            <section id="post" className="blog-post">
+                <h5> Day 2: 3/29/26</h5>
+                <img src="./images/rodeo.jpg" alt="rodeoDay" />
+                <p>
+                    One of the most classic Texan acitivies we had to do was going to a rodeo. Bulls, mutton bustin', 
+                    horseback riding, etc, etc. Everything you can think when you think rodeo? I saw it! And of course, it wouldn't be
+                    a Texan rodeo if there were cowboys and cowgirls alike. Boots, bootcut jeans, cowboy hats, flannel. Like a real desert movie!
+                </p>
+
+                <p>
+                    I felt a little underdressed but it was still fun. There were hotdog, popcorn, and fries to eat. 
+                     Of course there was beer. Did you know one of the most popular beer brands is Lonestar? Honestly 
+                     I didn't even know that brand existed until I went to Texas. And I don't like beer so I will never know 
+                     if it is popular for a reason.
+
+                </p>
+            </section>
+
+            <section id="post" className="blog-post">
+                <h5> Day 3: 3/30/26</h5>
+                <p>
+                    The next day was pretty slow! Finally I could sleep in after flight day. 
+                    Though, we didn't do nothin'! We went to a grocery store and bought ground beef, mustard, 
+                    onions, tomatoes, mayo, mustard, ketchup, lettuce... did you guess what we made? If you said burgers, you would be 
+                    100% right!! My brother makes one the best burgers I've had and no he did NOT pay me to type that!
+                </p>
+                <img src="./images/cookout.jpg" alt="cookout" />
+                <p>
+                    I haven't mentioned them before but my brother has two pets: a cat and a dog. If there's one thing 
+                    you should know about me, I love cats. Dogs...eh. The cat's name is Cici and the dogs name is Leon. 
+                    They both act like siblings, annoying and hitting each other. Classic.
+                </p>
+                <img src="./images/leon.png" alt="dog" />
+            </section>
+
+            <section id="post" className="blog-post">
+                <h5> Day 4: 3/31/26</h5>
+                <p>
+                    In the morning, I stayed inside for the day. Yipee lots of rest and being lazy. However, there was another adventure to be had at 3pm. Being in San Antonio, 
+                    Texas, there was no way we wouldn't go to the River Walk! Italy vibes for real! Along the river, there are 
+                    so many restaurants and gift shops to go to. Tourist attraction with overpriced items? Maybe. But that doesn't mean
+                     the view wasn't beautiful with beautiful weather!
+                </p>
+                <p>
+                    They also have these boats that goes through the river and the informed us 
+                    of the history of San Antonio. Did you know that besides English and Spanish, German 
+                    is the most spoken language? Wild. Though, I did notice a lot of German achitecture around 
+                    different towns. So that was something really cool to learn!
+                </p>
+                <img src="./images/riverwalk.png" alt="riverwalk" />
+            </section>
+            <section id="post" className="blog-post">
+                <h5> Day 5: 4/1/26</h5>
+                <p>
+                    April Fools Day! Unfortunately, this was the day I was leaving the great ol' state of Texas. 
+                    I had so much fun hanging out with my brother and seeing the place where he lived. I won't provide pictures of his hosue of course. 
+                    But, I was so glad I could spend that time with him.
+                </p>
+                <p>
+                    Now... I decided my last day meal would be something I had wanted since I got to Texas. 
+                    Something so delicious I think I could eat that everyday. Whataburger. A fun name for a fun burger. 
+                    It's very similar to In n Out, which we all know every Californian loves. The food feels very fresh and has the history written on the wall. 
+                    Then after such a good meal, it was off to the airport.
+                </p>
+                <img src="./images/whataburger.jpg" alt="whataburger" />
+            </section>
+        </main>
+        
+    );
+}
+export default Blog;
