6e30c37ceadeda32ef590534acd560cf151fdeaa
Sandra Principe <principesandra311@gmail.com>
2026-04-16 22:52:14 -0700

    Update portfolio

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter baename="SandraPrincipe/my-porfolio">
       <App />
     </BrowserRouter>
   </React.StrictMode>,
